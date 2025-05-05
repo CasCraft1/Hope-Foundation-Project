278ca9dd9616751553cdeacccdd036eb2215fc0d
Casey Craft <cascraft@unomaha.edu>
2025-05-04 21:05:21 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,11 @@ import re
 import streamlit as st
 
 
+
 #import and clean data
 
 def reviewclean(path: str):
-    reviewdata = pd.read_excel("UNO Service Learning Data Sheet De-Identified Version.xlsx")
+    reviewdata = pd.read_csv("data.csv")
     return(reviewdata)
 
 df = reviewclean("")
@@ -16,3 +17,4 @@ st.dataframe(df)
 #create dataframe with just "ready for review" data
 
 reviewdata = df.loc[:,"Application Signed?"]
+
