b2aca3c7e1887f11db1b609dd6887f6d71566dd0
CasCraft1 <mackmack0075@gmail.com>
2025-05-10 16:14:02 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ from pandas.api.types import (
 cwd = os.getcwd()
 files = [file for file in os.listdir(f"{cwd}/Clean Data/") if file.endswith('.csv')]
 path = files[0]
-data = pd.read_csv(files[0])
+data = pd.read_csv(f"{cwd}/Clean Data/{files[0]}")
 
 #st.set_page_config(layout="wide")
 
