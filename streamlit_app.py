3751a0398a215820035f453d906af88de7d9897d
CasCraft1 <mackmack0075@gmail.com>
2025-05-10 16:24:57 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ cwd = os.getcwd()
 files = [file for file in os.listdir(f"{cwd}/Clean Data/") if file.endswith('.csv')]
 path = files[0]
 f"{cwd}/Clean Data/{files[0]}"
-data = pd.read_csv(f"Clean Data/{files[0]}")
+data = pd.read_csv("Clean Data/cleandata5-2025.csv")
 
 #st.set_page_config(layout="wide")
 
