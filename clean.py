18846c7935056f9468a7680b1987437451c08690
Casey Craft <cascraft@unomaha.edu>
2025-05-09 21:28:40 -0500

    new stuff

--- clean.py
+++ clean.py
@@ -10,7 +10,7 @@ import sys
 
 
 # Get the filename from the workflow
-filename = os.getenv("FILE_NAME","")  # Get the first argument
+filename = os.getenv("FILE_NAME")  # Get the first argument
 
 if not filename:
     print("No file provided. Exiting.")
